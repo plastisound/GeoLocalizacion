0be572d6d62da2c5adfcab2ae2bbe215b9d1dd4d
plastisound <pisd14@gmail.com>
2013-07-20 16:51:03 -0500

    etc

--- actions.js
+++ actions.js
@@ -1,26 +1,26 @@
 $(document).ready(function(){
-    document.addEventListener("deciveready",
-        function(){
-            var onSuccess = function(position) { initialize(position.coords.latitude,position.coords.longitude);
-
-// onError Callback receives a PositionError object
-//
-function onError(error) {
-    alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
-}
-
+    document.addEventListener("deviceready", function(){
+        var onSuccess = function(position) {
+            initialize(position.coords.latitude,position.coords.longitude);
+        };
+        
+        // onError Callback receives a PositionError object
+        //
+        function onError(error) {
+            alert('code: '    + error.code    + '\n' +
+                  'message: ' + error.message + '\n');
+        }
 navigator.geolocation.getCurrentPosition(onSuccess, onError);
     }, false);
 });
 
-function initialize(lat,lon) {
+function initialize(lat,lon){
     //Posición del mapa
     var latlng = new google.maps.LatLng(lat, lon);
     var myOptions = {
-        zoom: 19,
+        zoom: 8,
         center: latlng,
-        mapTypeId: google.maps.MapTypeId.SATELLITE
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
     //Marcador
